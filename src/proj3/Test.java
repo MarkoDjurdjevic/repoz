67a5c1f643a784ee35f6b579904f512dab119819
Marko Djurdjevic <Djurdjevic.ra216.2019@uns.ac.rs>
2021-11-13 16:31:56 +0100

    dodao prvi fajl

---
+++ src/proj3/Test.java
@@ -0,0 +1,24 @@
+package proj3;
+
+import java.util.Scanner;
+
+public class Test {
+	
+	
+	
+	
+	
+	
+	
+//ctrl shift o sve da improtuje
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		String userInput = in.nextLine();
+		
+		System.out.println(userInput);
+
+		String[] parts = userInput.split(" ");
+	}
+
+}
